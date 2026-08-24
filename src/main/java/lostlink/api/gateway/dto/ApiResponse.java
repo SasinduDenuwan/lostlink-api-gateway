5709f829b3d075ec19ccf8787e8463a06590969a
Sasindu Denuwan <sasindudenuwan2006wpsk@gmail.com>
2026-08-24 21:57:52 +0530

    feat: implement Spring Cloud Gateway with JWT authentication and route validation

---
+++ src/main/java/lostlink/api/gateway/dto/ApiResponse.java
@@ -0,0 +1,14 @@
+package lostlink.api.gateway.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse {
+    private int status;
+    private String message;
+    private Object data;
+}
